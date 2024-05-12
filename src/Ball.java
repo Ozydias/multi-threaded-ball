11c003e4fb42b050626c9f54eb412c9cecf45325
xwb <2919927699@qq.com>
2024-05-12 22:37:40 +0800

    更新了Frame部分内容

--- src/Ball.java
+++ src/Ball.java
@@ -2,4 +2,14 @@
  * @author xwb
  */
 public class Ball {
+    private int x,y,r,vx,vy;
+    double distX,distY,distance;
+
+    public Ball(int x,int y,int r,int vx,int vy){
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.vx = vx;
+        this.vy = vy;
+    }
 }
--- src/Frame.java
+++ src/Frame.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 /**
  * @author xwb
@@ -7,14 +10,15 @@ import java.io.IOException;
 public class Frame extends JFrame {
     private int frameWidth,frameHeight;
     private JPanel jPanel;
-    public Frame(){
+    public static int mouseX,mouseY;
+    public Frame() throws IOException{
         super();
         init();
     }
 
-    public void init(){
-        this.frameWidth = 700;
-        this.frameHeight = 800;
+    private void init(){
+        this.frameWidth = 800;
+        this.frameHeight = 750;
         this.setSize(frameWidth,frameHeight);
         this.setTitle("多线程弹球游戏");
         //this.getContentPane().setBackground(Color.white);
@@ -26,17 +30,52 @@ public class Frame extends JFrame {
         jPanel.setBackground(Color.white);
         this.add(jPanel,BorderLayout.CENTER);
 
-        Image cur = createImage(40,40);
-        cur.getGraphics().fillRect(0,0,40,40);
-
-
-
-
-
-
         this.setDefaultCloseOperation(3);
         this.setVisible(true);
+
+
+//        Image cur = createImage(40,40);
+//        cur.getGraphics().fillRect(0,0,40,40);
+//
+//        Cursor cursor = Toolkit.getDefaultToolkit().createCustomCursor(cur,new Point(0,0),"A");
+//        jPanel.setCursor(cursor);
+        BufferedImage bufferedImage = new BufferedImage(40,40,BufferedImage.TYPE_INT_ARGB);
+
+        Graphics graphics2D = bufferedImage.createGraphics();
+        graphics2D.setColor(Color.black);
+        graphics2D.fillRect(0,0,40,40);
+        graphics2D.dispose();
+
+        Cursor cursor = Toolkit.getDefaultToolkit().createCustomCursor(bufferedImage,new Point(0,0),"A");
+        jPanel.setCursor(cursor);
+
+
+        addMouseMotionListener(new MouseMotionAdapter() {
+//            @Override
+//            public void mouseDragged(MouseEvent e) {
+//                super.mouseDragged(e);
+//                mouseX = e.getX();
+//                mouseY = e.getY();
+//            }
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+                mouseX = e.getX();
+                mouseY = e.getY();
+            }
+        });
+
     }
+
+    public void createBall(int sum){
+        for(int i=0;i<sum;i++){
+            addBall();
+        }
+    }
+    public void addBall(){
+
+    }
+
 
     public static void main(String agrs[]) throws IOException{
         new Frame();
